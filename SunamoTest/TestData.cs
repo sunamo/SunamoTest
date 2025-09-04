84276738095dd50a97e616a03308a99473677332
Radek Jancik <radek.jancik@usys.cz>
2025-09-04 21:54:26 +0200

    feat: Renamed variable by AI to better code quality

--- SunamoTest/TestData.cs
+++ SunamoTest/TestData.cs
@@ -1,3 +1,4 @@
+// Instance variables refactored according to C# conventions
 namespace SunamoTest;
 
 public class TestData
@@ -14,7 +15,7 @@ public class TestData
     public const string a2 = "a2";
     public const string wildcard = "*.cs";
 
-    public static readonly List<string> notSortedBySize = CAG.ToList<string>(ab, abc, a);
+    public static readonly List<string> notSortedBySize = [ab, abc, a];
 
     /// <summary>
     /// a,b
@@ -50,8 +51,8 @@ public class TestData
         listB = new List<string>(CA.ToListString2(b));
         listC = new List<string>(CA.ToListString2(c));
 
-        list04 = CAG.ToList<int>(0, 1, 2, 3, 4);
-        list59 = CAG.ToList<int>(5, 6, 7, 8, 9);
+        list04 = [0, 1, 2, 3, 4];
+        list59 = [5, 6, 7, 8, 9];
 
         _0To95By10 = new List<List<int>>(new List<int>[] { NH.GenerateIntervalInt(0, 9), NH.GenerateIntervalInt(10, 19), NH.GenerateIntervalInt(20, 29), NH.GenerateIntervalInt(30, 39), NH.GenerateIntervalInt(40, 49), NH.GenerateIntervalInt(50, 59), NH.GenerateIntervalInt(60, 69), NH.GenerateIntervalInt(70, 79), NH.GenerateIntervalInt(80, 89), NH.GenerateIntervalInt(90, 95) });
     }
@@ -60,10 +61,10 @@ public class TestData
     public const int two = 2;
     public const int three = 3;
 
-    public static readonly List<int> list12 = CA.ToInt(CAG.ToList<int>(1, 2));
-    public static readonly List<int> list34 = CA.ToInt(CAG.ToList<int>(3, 4));
-    public static readonly List<int> list1 = CA.ToInt(CAG.ToList<int>(1));
-    public static readonly List<int> list2 = CA.ToInt(CAG.ToList<int>(2));
+    public static readonly List<int> list12 = CA.ToInt([1, 2]);
+    public static readonly List<int> list34 = CA.ToInt([3, 4]);
+    public static readonly List<int> list1 = CA.ToInt([1]);
+    public static readonly List<int> list2 = CA.ToInt([2]);
     public static readonly List<string> list100Items = LinearHelper.GetStringListFromTo(0, 99);
     public static readonly List<string> list10Items = LinearHelper.GetStringListFromTo(0, 9);
     public static readonly List<int> _0To100 = NH.GenerateIntervalInt(0, 100);
--- SunamoTest/TestHelper.cs
+++ SunamoTest/TestHelper.cs
@@ -1,3 +1,4 @@
+// Instance variables refactored according to C# conventions
 namespace SunamoTest;
 
 using System.IO;
